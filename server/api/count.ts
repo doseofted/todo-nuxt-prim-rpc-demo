7a120da99eccd6d46395edf8c79868823332e1d8
Ted Klingenberg <ted@doseofted.com>
2023-06-24 22:37:22 -0400

    Removed Nitro routes

--- server/api/count.ts
+++
@@ -1,7 +0,0 @@
-import { todo } from "../rpc";
-
-export default defineEventHandler(async (event) => {
-  const count = await todo.count();
-  setHeader(event, "Content-Type", "text/plain");
-  return count;
-});
--- server/api/list.ts
+++
@@ -1,7 +0,0 @@
-import { todo } from "../rpc";
-
-export default defineEventHandler(async (event) => {
-  const query = getQuery(event);
-  const { page = 1, pageSize = 10 } = query;
-  return todo.list(Number(page), Number(pageSize));
-});
