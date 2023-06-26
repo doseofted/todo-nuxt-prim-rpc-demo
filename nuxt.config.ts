e447ed001fc99cf29585856056dc155559438ee6
Ted Klingenberg <ted@doseofted.com>
2023-06-26 15:47:50 -0400

    Added prevent import tool

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
+import preventImport, {
+  BuildModifyMethod,
+} from "@doseofted/prim-rpc-tooling/build";
+import path from "node:path";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-icon"],
   vite: {
+    plugins: [
+      preventImport.vite({
+        name: path.join(__dirname, "functions"),
+        method: BuildModifyMethod.RunTimeProcessCheck,
+      }),
+    ],
     build: {
       rollupOptions: {
         external: [
